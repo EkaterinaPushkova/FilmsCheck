e327d21b9fa56d523318b12bac711197db68b56f
Ekaterina <pushkova1666@gmail.com>
2021-03-21 19:38:17 +0300

    done - filing obj pDB.movies

--- main_script.js
+++ main_script.js
@@ -1,4 +1,4 @@
-let numberOfFilms = prompt('How many films have you watched?', [19]);
+let numberOfFilms = +prompt('How many films have you watched?', [19]);
 document.write('the number of movies you watched - ', numberOfFilms);
 
 const personalMoviesDB = {
@@ -8,3 +8,15 @@ const personalMoviesDB = {
     geners: [],
     privat: false
 };
+
+const   a = prompt("The last movie you watched?"),
+        b = +prompt("What's your rate for this?"),
+        c = prompt("The last movie you watched?"),
+        d = +prompt("What's your rate for this?");
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+
+
+console.log(personalMoviesDB); 
