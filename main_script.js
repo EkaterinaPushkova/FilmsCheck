6100cd878ff2fd814cb3cf85917c53ff2af299c2
Ekaterina <pushkova1666@gmail.com>
2021-03-21 19:09:55 +0300

    done second task - init obj personalMoviesDB

--- main_script.js
+++ main_script.js
@@ -1,2 +1,10 @@
 let numberOfFilms = prompt('How many films have you watched?', [19]);
 document.write('the number of movies you watched - ', numberOfFilms);
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
