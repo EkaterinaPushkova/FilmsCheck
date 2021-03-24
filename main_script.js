d6892966523c18c7b3d674d5a597fd5417c132b7
Ekaterina <pushkova1666@gmail.com>
2021-03-24 23:22:08 +0300

    try init an answer filter from second task

--- main_script.js
+++ main_script.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 let numberOfFilms = +prompt('How many films have you watched?', [19]);
 document.write('the number of movies you watched - ', numberOfFilms);
@@ -7,18 +7,21 @@ const personalMoviesDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    geners: [],
+    genres: [],
     privat: false
 };
 
-
-for (let i=0; i < 2; i++) {
-    const  a = prompt("The last movie you watched?"),
-           b = +prompt("What's your rate for this?");
-
-    personalMoviesDB.movies[a] = b;
-}
-
+    for (let i=0; i < 2; i++) {
+        if (a&&b == "", a.length > 5, a&&b == false) {
+            const  a = prompt("The last movie you watched?"),
+            b = +prompt("What's your rate for this?");
+ 
+            personalMoviesDB.movies[a] = b;
+        }else{
+            continue;
+        }
+        
+    }
 
 
 
