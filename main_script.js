5a5b81976b68ed9bc8f26a31cabb6eb26abd7af2
Ekaterina <pushkova1666@gmail.com>
2021-03-31 00:45:53 +0300

    all actions are recreated as functions

--- main_script.js
+++ main_script.js
@@ -1,9 +1,16 @@
 // "use strict";
 
-let numberOfFilms = +prompt('How many films have you watched?', [11]);
-// document.write('the number of movies you watched - ', numberOfFilms);
-// alert('the number of movies you watched - ', numberOfFilms);
-alert(`the number of movies you watched - ${numberOfFilms}`);
+let numberOfFilms;
+
+
+function countFilms(){
+        numberOfFilms = +prompt('How many films have you watched?', [11]);
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('How many films have you watched?', [11]);
+    }
+}
+countFilms();
+console.log(typeof(numberOfFilms));
 
 const personalMoviesDB = {
     count: numberOfFilms,
@@ -13,11 +20,13 @@ const personalMoviesDB = {
     privat: false
 };
 
+
+function rememberMyFilms(){
     for (let i=0; i < 2; i++) {
             const  a = prompt("The last movie you watched?"),
             b = +prompt("What's your rate for this?");
 
-            if (a != "" && b != "" && a.length < 5 && a != null && b != null) {
+            if (a != "" || b != "" && a.length < 5 && a != null && b != null) {
             personalMoviesDB.movies[a] = b;
         }else{
             i--;  
@@ -25,21 +34,28 @@ const personalMoviesDB = {
     }
 
 console.log(personalMoviesDB); 
-
-switch ( personalMoviesDB.count){
-    case 2: 
-    //  document.write("You are is a normally viewer");
-        alert("You are is a normally viewer");
-        break;
-    case 4:
-        alert("You are is a movie fan");
-        break;
-    case 8:
-        alert("You are is a CRAZY MEGA MOVIE HERO");
-        break;
-    default:
-       alert("error");
 }
+rememberMyFilms();
+
+
+function getStatus(){
+    switch (personalMoviesDB.count){
+        case 2: 
+        //  document.write("You are is a normally viewer");
+            alert("You are is a normally viewer");
+            break;
+        case 4:
+            alert("You are is a movie fan");
+            break;
+        case 8:
+            alert("You are is a CRAZY MEGA MOVIE HERO");
+            break;
+        default:
+           alert("error");
+    }
+    
+}
+getStatus();
 
 // if (personalMovieDB.count < 10) {
 //     console.log("Просмотрено довольно мало фильмов");
