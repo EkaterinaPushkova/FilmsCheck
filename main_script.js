6c6cca2b61fcd514fffe647adc90e297a05ea65c
Ekaterina <pushkova1666@gmail.com>
2021-03-31 23:48:26 +0300

    done 1.24 task - modified object 'personalMoviesDB': functions are reinitialized as methods of this object

--- main_script.js
+++ main_script.js
@@ -2,67 +2,73 @@
 
 let numberOfFilms;
 
-
-function countFilms(){
-        numberOfFilms = +prompt('How many films have you watched?', [11]);
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt('How many films have you watched?', [11]);
-    }
-}
-countFilms();
-console.log(typeof(numberOfFilms));
-
 const personalMoviesDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-
-function rememberMyFilms(){
-    for (let i=0; i < 2; i++) {
-            const  a = prompt("The last movie you watched?"),
-            b = +prompt("What's your rate for this?");
-
-            if (a != "" || b != "" && a.length < 5 && a != null && b != null) {
-            personalMoviesDB.movies[a] = b;
-        }else{
-            i--;  
+    privat: false,
+    countFilms: function (){
+        numberOfFilms = +prompt('How many films have you watched?', [11]);
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('How many films have you watched?', [11]);
+        }
+    },
+    rememberMyFilms: function (){
+        for (let i=0; i < 2; i++) {
+                const  a = prompt("The last movie you watched?"),
+                b = +prompt("What's your rate for this?");
+    
+                if (a != "" || b != "" && a.length < 5 && a != null && b != null) {
+                personalMoviesDB.movies[a] = b;
+            }else{
+                i--;  
+            }
+        }
+    },
+    getStatus: function (){
+        switch (personalMoviesDB.count){
+            case 2: 
+            //  document.write("You are is a normally viewer");
+                alert("You are is a normally viewer");
+                break;
+            case 4:
+                alert("You are is a movie fan");
+                break;
+            case 8:
+                alert("You are is a CRAZY MEGA MOVIE HERO");
+                break;
+            default:
+               alert("error");
+        }
+    },
+    showMyDB: function(){
+        if (personalMoviesDB.privat == false){
+            console.log(personalMoviesDB);
+        }
+     },
+     writeYourGenres: function (){
+        for(i = 1; i <=3 ; i++){
+           personalMoviesDB.genres[i-1] = prompt(`Your favorite genre at number${i}`, '');
         }
     }
 
-console.log(personalMoviesDB.movies); 
-}
+};
+
+
+// countFilms();
+
+// getStatus();
+
+// showMyDB();
+
+// console.log(typeof(numberOfFilms));
+
+// console.log(personalMoviesDB.movies);
+
+// console.log(personalMoviesDB.genres);
+
 // rememberMyFilms();
-
-
-function getStatus(){
-    switch (personalMoviesDB.count){
-        case 2: 
-        //  document.write("You are is a normally viewer");
-            alert("You are is a normally viewer");
-            break;
-        case 4:
-            alert("You are is a movie fan");
-            break;
-        case 8:
-            alert("You are is a CRAZY MEGA MOVIE HERO");
-            break;
-        default:
-           alert("error");
-    }
-    
-}
-getStatus();
-
-function showMyDB(){
-   if (personalMoviesDB.privat == false){
-       console.log(personalMoviesDB);
-   }
-}
-showMyDB();
 
 //more beauty ex:
 // function showMyDB(hidden){
@@ -71,12 +77,7 @@ showMyDB();
 //     }
 //  }
 
-function writeYourGenres(){
-    for(i = 1; i <=3 ; i++){
-       personalMoviesDB.genres[i-1] = prompt(`Your favorite genre at number${i}`, '');
-    }
-}
-console.log(personalMoviesDB.genres);
+
 
 // if (personalMovieDB.count < 10) {
 //     console.log("Просмотрено довольно мало фильмов");
